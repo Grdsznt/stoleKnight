da97d0d846cf8665c504dfadb917766cdd69f096
AndyFeng070205 <150149712+AndyFeng070205@users.noreply.github.com>
2024-05-30 22:38:19 -0400

    mouse drag and keyboard change direction

--- GameWorld.java
+++ GameWorld.java
@@ -10,14 +10,44 @@ public class GameWorld extends World
 {
     
     ArrayList<Tile> grid;
+    MouseInfo mouse;
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
+    
     public GameWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1280, 720, 1); 
         
+    }
+    
+    public void act() {
+        mouse = Greenfoot.getMouseInfo();
+    }
+    
+    public int getMouseX() {
+        return mouse.getX();
+    }
+    
+    public int getMouseY() {
+        return mouse.getY();
+    }
+    
+    public boolean isMousePressed(Actor obj) {
+        return Greenfoot.mousePressed(obj);
+    }
+    
+    public boolean isMouseDragged(Actor obj) {
+        return Greenfoot.mouseDragged(obj);
+    }
+    
+    public boolean hasMouseDragEnded(Actor obj) {
+        return Greenfoot.mouseDragEnded(obj);
+    }
+    
+    public boolean isMouseClicked(Actor obj) {
+        return Greenfoot.mouseClicked(obj);
     }
 }
