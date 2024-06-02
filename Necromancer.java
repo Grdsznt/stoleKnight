4414f3a3aca10b2ec3b915f11dfe7fe2c2e931f7
Grdsznt <edwin7x7@gmail.com>
2024-06-02 16:02:07 -0400

    c

---
+++ Necromancer.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Necromancer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Necromancer extends Enemy
+{
+    /**
+     * Act - do whatever the Necromancer wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+    
+    public Necromancer() {
+        super(300, 4, 60);
+    }
+}
